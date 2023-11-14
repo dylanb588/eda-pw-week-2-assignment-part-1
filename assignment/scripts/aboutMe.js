5e5072d9ab38b0ff307bf955bd1c942e1a2ff4fa
Dylan Brown <dylanbrown588@gmail.com>
2023-11-14 17:32:54 -0600

    added luckyNumber and consolelog

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -7,10 +7,11 @@ let lastName = 'Brown';
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
 let fullName = firstName + " " + lastName ;
-console.log(fullName)
+console.log(fullName);
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
-
+let luckyNumber = 5;
+console.log(luckyNumber);
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
 //     'My name is (full name), and I think (lucky number) is a winner!'.
