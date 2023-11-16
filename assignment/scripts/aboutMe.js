2ca8c7a8dc06b31b47ebe2ff0f2740b5bb00d664
Dylan Brown <dylanbrown588@gmail.com>
2023-11-15 18:37:20 -0600

    added friendsPets and const allowed pets

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -32,11 +32,11 @@ console.log(food);
 let pets = 1;
 console.log(pets);
 // 9 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 2;
 // 10 - Add two pets to your `pets` variable
-
+pets += 2;
 // 11 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets = 2;
 // 12 - Create a variable called `result`. Create a conditional:
 //      if adventurous is true, set `result` to be "Adventures are great!", 
 //      if it's not true,  set `result` to be "How about we stay home?"
